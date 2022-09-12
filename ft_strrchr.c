b5267b9133aeee200280fa87ca39422efd2263e1
threedp.png <davypaulinocbjr@outlook.com>
2022-09-12 18:22:31 +0200

    Implementação da função strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 17:36:03 by coder             #+#    #+#             */
+/*   Updated: 2022/09/12 17:44:05 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	size_t	rev_i;
+
+	rev_i = ft_strlen(str) + 1;
+	while (rev_i > 0)
+	{
+		if (str[rev_i] == (char *) c)
+			return ((char *) &str[rev_i]);
+		rev_i--;
+	}
+	return (0);
+}
