bb4b9d6c06e0aa5af9def800c68208e4b2e6c924
threedp.png <davypaulinocbjr@outlook.com>
2022-09-12 18:24:40 +0200

    Alterações dos arquivos disponiveis para exec e inclusão dos prototipos até o strlcat

--- libft.h
+++ libft.h
@@ -1,4 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 21:23:52 by coder             #+#    #+#             */
+/*   Updated: 2022/09/12 16:59:21 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
-	int	isalpha(int c);
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+size_t	ft_strlen(const char *str);
+void	*ft_memset(void *str, int c_fill, size_t n_bytes);
+void	ft_bzero(void *str, size_t n_bytes);
+void	*ft_memcpy(void *dest, const void *src, size_t n_bytes);
+void	*ft_memmove(void *dest, const void *src, size_t n_bytes);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
 #endif
