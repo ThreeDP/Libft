b51430ea62677a36253e45bd87ab43987ed42f49
threedp.png <davypaulinocbjr@outlook.com>
2022-09-17 21:11:37 +0200

    Correção da função strlcpy: número de caracteres a ser copiado

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,24 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 16:12:08 by coder             #+#    #+#             */
-/*   Updated: 2022/09/12 17:31:24 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/17 21:07:00 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < size || src[i])
+	if (size == 0)
+		return (ft_strlen(src));
+	while (i < (size - 1) && src[i])
 	{
-		dst[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
+	dest[i] = '\0';
 	return (ft_strlen(src));
 }
