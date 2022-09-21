d482463cbbe734cd56069bf8170a823ad9c1225d
threedp.png <davypaulinocbjr@outlook.com>
2022-09-21 19:02:04 +0200

    Implementation of the function ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:35 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 07:52:01 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/21 18:45:42 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,21 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
+	size_t	j;
 	char	*cached_big;
 	char	*cached_little;
 
+	i = 0;
 	cached_big = (char *)big;
 	cached_little = (char *)little;
-	if (ft_strlen(little) == 0)
-		return (cached_big);
-	i = 0;
 	while (i < len)
 	{
-		return (cached_big);
-		i++;
+		j = 0;
+		while (cached_big[i + j] == cached_little[j])
+			j++;
+		if (cached_little[j] == '\0')
+			return (&cached_big[i]);
+		i += 1 + j;
 	}
 	return (0);
 }
