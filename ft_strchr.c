fc310b7db8fdd55be11fd93ab57eb575a15adf9b
threedp.png <davypaulinocbjr@outlook.com>
2022-09-21 14:11:19 +0200

    Correct the implementation of function ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,24 +6,26 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:00 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 07:37:02 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/21 13:58:24 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *str, int c)
 {
 	size_t	i;
+	size_t	size;
+	char	*cached_str;
 
 	i = 0;
-	while (str[i])
+	size = ft_strlen(str);
+	cached_str = (char *)str;
+	while (i <= size)
 	{
-		if (str[i] == c)
-			return (&((char *)str)[i]);
+		if (cached_str[i] == c)
+			return (&cached_str[i]);
 		i++;
 	}
-	if (str[i] == c)
-		return (&((char *)str)[i]);
 	return (0);
 }
