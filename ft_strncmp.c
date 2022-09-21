1da7986684c5236812ec09f842a9f356f5d10021
threedp.png <davypaulinocbjr@outlook.com>
2022-09-21 17:35:25 +0200

    Implement the function strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:28 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 07:37:30 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:28:51 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	pos;
 
 	pos = 0;
-	while (pos <= n)
+	while (pos < n && (s1[pos] != '\0' || s2[pos] != '\0'))
 	{
 		if (s1[pos] != s2[pos])
 			return (s1[pos] - s2[pos]);
