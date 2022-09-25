66f2fcf5b6ca9854615d385ab2983bb65aa9577e
threedp.png <davypaulinocbjr@outlook.com>
2022-09-25 16:33:18 +0200

    correct the header

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,11 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:36:47 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 07:44:58 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/25 07:43:22 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n_bytes)
 {
@@ -19,10 +19,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n_bytes)
 
 	cached_dest = (unsigned char *)dest;
 	cached_src = (unsigned char *)src;
-	while (n_bytes > 0)
-	{
+	if (cached_dest < cached_src)
+		return (ft_memcpy(dest, src, n_bytes));
+	while (n_bytes-- > 0)
 		cached_dest[n_bytes] = cached_src[n_bytes];
-		n_bytes--;
-	}
 	return (cached_dest);
 }
