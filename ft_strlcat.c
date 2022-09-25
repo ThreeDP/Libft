72738951d959bd58f77f9e1601af6c204299f37b
threedp.png <davypaulinocbjr@outlook.com>
2022-09-25 16:33:56 +0200

    Implement function strlcat with error

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:07 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 07:37:08 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/25 13:10:51 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	pos;
+	size_t			i;
+	size_t			pos;
+	unsigned char	*cached_src;
 
 	i = 0;
 	pos = ft_strlen(dst);
-	while ((pos + i) < size || src[i])
+	cached_src = (unsigned char *)src;
+	while (pos + 1 < size && cached_src[i])
 	{
-		dst[pos + i] = src[i];
+		dst[pos] = cached_src[i];
+		pos++;
 		i++;
 	}
-	dst[pos + i] = '\0';
-	return (pos);
+	dst[pos] = '\0';
+	return (ft_strlen(dst) + ft_strlen(src));
 }
