399c79c112c4f567e90cc7666939939d07cda3c2
threedp.png <davypaulinocbjr@outlook.com>
2022-09-25 05:09:04 +0200

    correct the negative numbers with unsigned char function ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:28 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/24 18:16:15 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/25 03:50:47 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	pos;
+	size_t			pos;
+	unsigned char	*cached_s1;
+	unsigned char	*cached_s2;
 
 	pos = 0;
-	while (pos < n && (s1[pos] || s2[pos]))
+	cached_s1 = (unsigned char *)s1;
+	cached_s2 = (unsigned char *)s2;
+	while (pos < n && (cached_s1[pos] || cached_s2[pos]))
 	{
-		if (s1[pos] != s2[pos])
-			return (s1[pos] - s2[pos]);
+		if (cached_s1[pos] != cached_s2[pos])
+			return (cached_s1[pos] - cached_s2[pos]);
 		pos++;
 	}
 	return (0);
