a1c480f49f41e4fd287102344b81218c43e4f64e
threedp.png <davypaulinocbjr@outlook.com>
2022-09-25 05:11:28 +0200

    Correct character with signal, remove unsigned char and add char.

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:36:25 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 07:48:59 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/09/25 04:11:55 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	*ft_memchr(const void *str, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*cached_str;
+	size_t	i;
+	char	*cached_str;
+	char	cached_c;
 
 	i = 0;
-	cached_str = (unsigned char *)str;
+	cached_str = (char *)str;
+	cached_c = (char)c;
 	while (i < n)
 	{
-		if (cached_str[i] == c)
+		if (cached_str[i] == cached_c)
 			return (&cached_str[i]);
 		i++;
 	}
