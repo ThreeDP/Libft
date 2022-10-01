a9f0a69a58b9a1ae519d9bff05b6036d4782e671
threedp.png <davypaulinocbjr@outlook.com>
2022-10-01 04:24:04 +0200

    others

---
+++ add_one.c
@@ -0,0 +1,6 @@
+#include "libft.h"
+
+char add_one(unsigned int iter, char c)
+{
+	return (iter + c);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 15:00:24 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/25 16:03:25 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/10/01 03:47:12 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*new_sub;
 
 	new_sub = (char *) ft_calloc(len, sizeof(char) + 1);
-	if (!(start > ft_strlen(s)))
-		ft_strlcpy(new_sub, &s[start], len + 1);
+	if (!new_sub)
+		return (NULL);
+	if (start < ft_strlen(s))
+		ft_strlcpy(new_sub, &s[start], len);
 	return (new_sub);
 }
