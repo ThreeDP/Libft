dbb9efa8133226f22ac62dfd1526c369b9af6637
threedp.png <davypaulinocbjr@outlook.com>
2022-10-04 01:34:05 +0200

    Dei uma de juvenil e esqueci de converte o caracter de comparação em um char, outro ato juvenil

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,12 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:00 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 13:58:24 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/10/02 01:11:45 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strchr(const char *str, int c)
 {
@@ -21,9 +22,9 @@ char	*ft_strchr(const char *str, int c)
 	i = 0;
 	size = ft_strlen(str);
 	cached_str = (char *)str;
-	while (i <= size)
+	while (i <= size + 1)
 	{
-		if (cached_str[i] == c)
+		if (cached_str[i] == (char)c)
 			return (&cached_str[i]);
 		i++;
 	}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:42 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/21 15:56:57 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/10/02 01:15:58 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ char	*ft_strrchr(const char *str, int c)
 {
 	size_t	rev_i;
 	char	*cached_str;
+	char	cached_c;
 
+	cached_c = (char)c;
 	rev_i = ft_strlen(str);
 	cached_str = (char *)str;
-	while (rev_i != 0 && cached_str[rev_i] != c)
+	while (rev_i != 0 && cached_str[rev_i] != cached_c)
 		rev_i--;
-	if (cached_str[rev_i] == c)
+	if (cached_str[rev_i] == cached_c)
 		return (&cached_str[rev_i]);
 	return (0);
 }
