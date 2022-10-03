a9a6f1c13dda77dfd46800ab906b88418ccb26b0
threedp.png <davypaulinocbjr@outlook.com>
2022-10-04 01:21:06 +0200

    Correct the error in the function: empty string was returning null

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 07:37:35 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/09/25 16:04:49 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/10/04 01:14:27 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	len_little;
 	char	*cached_big;
 	char	*cached_little;
+	size_t	l = -1;
 
 	i = 0;
 	cached_big = (char *)big;
 	cached_little = (char *)little;
 	len_little = ft_strlen(cached_little);
-	while (i < len)
+	if (len_little == 0)
+		return (cached_big);
+	while (i <= len && cached_big[i])
 	{
 		if (!ft_strncmp(&cached_big[i], cached_little, len_little)
-			&& len - i > len_little)
+			&& len - i >= len_little)
 			return (&cached_big[i]);
 		i++;
 	}
