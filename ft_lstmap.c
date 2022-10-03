860a7c9fd6f56a72142f73d8ef4989ee98005646
threedp.png <davypaulinocbjr@outlook.com>
2022-10-04 01:37:42 +0200

    Falta metade da ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,7 +2,17 @@
 
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    f(lst);
-    del(0);
-    return (0);
+	size_t	size;
+	t_list	*ptr;	
+
+	size = ft_lstsize(lst);
+	ptr = (t_list *) ft_calloc(size + 1, sizeof(lst));
+	if (!ptr)
+		return (NULL);
+	while (lst)
+	{
+		f(lst -> content);
+		lst = lst -> next;
+	}
+    return (ptr);
 }
