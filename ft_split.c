a262bab3a564fada336b77b16254503ebd3a87d8
threedp.png <davypaulinocbjr@outlook.com>
2022-10-04 22:31:55 +0200

    v2 split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 20:50:43 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/10/03 23:41:48 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/10/04 22:28:55 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,77 +15,57 @@
 
 static char	**find_words(char const *s, char c)
 {
-	size_t		amount;
-	char	*ptr;
-	char	*ptr_n;
-	char	*start_w;
-	
-	amount = 0;
-	ptr = (char *) s;
-	ptr_n = (char *) s;
-	start_w = (char *) s;
-	while (1)
-	{
-		
-		ptr = ft_strchr(ptr, c);
-		if (ptr && c != 0)
-		{
-			if (ptr - start_w > 1)
-				amount++;
-			if (ptr == 0)
-				break ;
-			ptr++;
-		}
-		else
-		{
-			ptr_n = ft_strchr(ptr_n, '\0');
-			if (ptr_n - start_w > 1)
-				amount++;
-		}
-		if (!*ptr_n)
-			break ;
-		start_w = ptr;
-	}
-	return ((char **) ft_calloc(amount + 1, sizeof(char *)));
+    size_t    amount;
+    char    *start;
+    char    *next_point;
+    
+    amount = 0;
+    start = (char *) s;
+    next_point = NULL;
+    while (1)
+    {
+        next_point = ft_strchr(start, c);
+        if (next_point - start > 1 && c != '\0')
+            amount++;
+        else if (ft_strchr(start, '\0') - start > 1 && (next_point == 0 || c == 0))
+        {
+            amount++;
+            break;
+        }
+        else if (next_point == 0)
+          break;
+        start = next_point + 1;
+    }
+    return ((char **) ft_calloc(amount + 1, sizeof(char *)));
 }
 
 static void	split_words(char **rows, char const *text, char c)
 {
 	int		i;
-	char	*start_w;
-	char	*end_w;
-	char	*end_n;
 	int		size;
-	
+	char	*start_w;
+
 	i = 0;
 	start_w = (char *) text;
-	end_w = (char *) text;
-	end_n = (char *) text;
 	while (1)
 	{
-		end_w = ft_strchr(end_w, c);
-		if (end_w && c != 0)
+		if (ft_strchr(start_w, c) && c != 0)
 		{
-			size = (end_w - start_w);
+			size = (ft_strchr(start_w, c) - start_w);
 			if (size > 1)
 			{
 				rows[i] = ft_substr(text, start_w - text, size);
 				i++;
 			}
-			if (end_w == 0)
-				break ;
-			end_w++;
 		}
-		else
+		else if (ft_strchr(start_w, '\0'))
 		{
-			end_n = ft_strchr(end_n, '\0');
-			size = (end_n - start_w);
+			size = (ft_strchr(start_w, '\0') - start_w);
 			if (size > 1)
 				rows[i] = ft_substr(text, start_w - text, size);
+			break;
 		}
-		if (!*end_n)
-			break ;
-		start_w = end_w;
+		start_w = ft_strchr(start_w, c) + 1;
 	}
 }
 
