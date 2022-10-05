cd1798de9e7ebd7b3a1aa859bebea300bea6c635
threedp.png <davypaulinocbjr@outlook.com>
2022-10-05 02:01:26 +0200

    Implemented the function split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 20:50:43 by dapaulin          #+#    #+#             */
-/*   Updated: 2022/10/04 22:28:55 by dapaulin         ###   ########.fr       */
+/*   Updated: 2022/10/05 00:38:03 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,66 +15,67 @@
 
 static char	**find_words(char const *s, char c)
 {
-    size_t    amount;
-    char    *start;
-    char    *next_point;
-    
-    amount = 0;
-    start = (char *) s;
-    next_point = NULL;
-    while (1)
-    {
-        next_point = ft_strchr(start, c);
-        if (next_point - start > 1 && c != '\0')
-            amount++;
-        else if (ft_strchr(start, '\0') - start > 1 && (next_point == 0 || c == 0))
-        {
-            amount++;
-            break;
-        }
-        else if (next_point == 0)
-          break;
-        start = next_point + 1;
-    }
-    return ((char **) ft_calloc(amount + 1, sizeof(char *)));
+	size_t	amount;
+	char	*start;
+	char	*next_point;
+
+	amount = 0;
+	start = (char *) s;
+	next_point = NULL;
+	while (1)
+	{
+		next_point = ft_strchr(start, c);
+		if (next_point - start > 0 && c != '\0')
+			amount++;
+		else if (ft_strchr(start, '\0') - start > 0
+			&& (next_point == 0 || c == 0))
+		{
+			amount++;
+			break ;
+		}
+		else if (next_point == 0)
+			break ;
+		start = next_point + 1;
+	}
+	return ((char **) ft_calloc(amount + 1, sizeof(char *)));
 }
 
 static void	split_words(char **rows, char const *text, char c)
 {
 	int		i;
 	int		size;
-	char	*start_w;
+	char	*start;
+	char	*next_point;
 
 	i = 0;
-	start_w = (char *) text;
+	next_point = NULL;
+	start = (char *) text;
 	while (1)
 	{
-		if (ft_strchr(start_w, c) && c != 0)
+		next_point = ft_strchr(start, c);
+		size = next_point - start;
+		if (size > 0 && c != '\0')
+			rows[i++] = ft_substr(text, start - text, size);
+		else if (ft_strchr(start, '\0') - start > 0
+			&& (next_point == 0 || c == 0))
 		{
-			size = (ft_strchr(start_w, c) - start_w);
-			if (size > 1)
-			{
-				rows[i] = ft_substr(text, start_w - text, size);
-				i++;
-			}
+			rows[i++] = ft_substr(text, start - text, size);
+			break ;
 		}
-		else if (ft_strchr(start_w, '\0'))
-		{
-			size = (ft_strchr(start_w, '\0') - start_w);
-			if (size > 1)
-				rows[i] = ft_substr(text, start_w - text, size);
-			break;
-		}
-		start_w = ft_strchr(start_w, c) + 1;
+		else if (next_point == 0)
+			break ;
+		start = next_point + 1;
 	}
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**splitted;
-	
+
+	if (!s || !*s)
+		return (ft_calloc(1, sizeof(char **)));
 	splitted = find_words(s, c);
-	if(splitted)
+	if (splitted)
 		split_words(splitted, s, c);
 	return (splitted);
 }
