2612e05f23552585981a1ae6b88e82200496b3ff
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-12-08 10:30:27 -0300

    Test: New test for the ft_putchar_fd

--- minunit/test/ft_putchar_fd_test.c
+++ minunit/test/ft_putchar_fd_test.c
@@ -57,6 +57,42 @@ MU_TEST_SUITE(passing_a_char_D_should_be_1)
 	mu_assert_string_eq(exp_str, str);
 }
 
+MU_TEST_SUITE(passing_a_char_backslash_0_should_be_empty)
+{
+	//ARRANGE
+	int         fd;
+	int         size;
+	char        *str;
+	char        *file               = "./files/empty";
+	char        input               = 'D';
+	char        *exp_str            = "";
+	int         exp_size            = 0;
+
+	//ACT
+	setup(file, &fd);
+	size = ft_putchar_fd(input, fd);
+
+	//ASSERT
+	str = unset(file, fd);
+	mu_assert_int_eq(exp_size, size);
+	mu_assert_string_eq(exp_str, str);
+}
+
+MU_TEST_SUITE(passing_a_invalid_fd_should_be_minus_1)
+{
+	//ARRANGE
+	int         size;
+	int         fd                  = 1001;
+	char        input               = 'D';
+	int         exp_size            = -1;
+
+	//ACT
+	size = ft_putchar_fd(input, fd);
+
+	//ASSERT
+	mu_assert_int_eq(exp_size, size);
+}
+
 MU_TEST_SUITE(test_suite)
 {
 	MU_RUN_TEST(passing_a_char_D_should_be_1);
