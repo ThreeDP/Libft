71617bcf8884680fd549dec470b9a5b3b9ff10cc
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-12-08 17:40:06 -0300

    Feacture: new function ft_puthex, put in the fd the valor in the input in hex

--- srcs/libft.h
+++ srcs/libft.h
@@ -60,6 +60,7 @@ int			ft_putchar_fd(char c, int fd);
 int			ft_putstr_fd(char *s, int fd);
 int			ft_putendl_fd(char *s, int fd);
 int			ft_putnbr_fd(int nb, int fd);
+int			ft_puthex_fd(unsigned long nb, int fd, char *case);
 
 // Bonus	==========================================================
 t_list		*ft_lstnew(void *content);
