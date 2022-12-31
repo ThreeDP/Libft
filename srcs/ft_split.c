941d52e3bc5dff4d4309526fee399d4b3baf99b9
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2022-12-31 01:09:42 -0300

    FIX: Segmentation Fault when split path when passing a file path for some reason it was giving segmentation fault.

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -12,69 +12,75 @@
 
 #include "libft.h"
 
-static char	**find_words(char const *s, char c)
+static int  next_bit(char **s, char *n)
 {
-	size_t	amount;
-	char	*start;
-	char	*next_point;
-
-	amount = 0;
-	start = (char *) s;
-	next_point = NULL;
-	while (1)
-	{
-		next_point = ft_strchr(start, c);
-		if (next_point - start > 0 && c != '\0')
-			amount++;
-		else if (ft_strchr(start, '\0') - start > 0
-			&& (next_point == 0 || c == 0))
-		{
-			amount++;
-			break ;
-		}
-		else if (next_point == 0)
-			break ;
-		start = next_point + 1;
-	}
-	return ((char **) ft_calloc(amount + 1, sizeof(char *)));
+    if (n && (n - (*s)) == 0)
+    {
+		(*s)++;
+        return (1);
+    }
+    return (0);
 }
 
-static void	split_words(char **rows, char const *text, char c)
+size_t count_words(char *s, char c, size_t amount)
 {
-	int		i;
-	int		size;
-	char	*start;
-	char	*next_point;
+    char    *next;
 
-	i = 0;
-	next_point = NULL;
-	start = (char *) text;
-	while (1)
-	{
-		next_point = ft_strchr(start, c);
-		size = next_point - start;
-		if (size > 0 && c != '\0')
-			rows[i++] = ft_substr(text, start - text, size);
-		else if (ft_strchr(start, '\0') - start > 0
-			&& (next_point == 0 || c == 0))
-		{
-			rows[i++] = ft_substr(text, start - text, size);
-			break ;
-		}
-		else if (next_point == 0)
-			break ;
-		start = next_point + 1;
-	}
+    next = NULL;
+    while (1)
+    {
+        next = ft_strchr(s, c);
+        if (next && (next - s) > 0 && ft_strlen(s) > (size_t)(next - s))
+        {
+            s += (next - s) + 1;
+            amount++;
+            continue ;
+        }
+        if (next_bit(&s, next))
+            continue ;
+        if (ft_strlen(s))
+            amount++;
+        break ;
+    }
+    return (amount);
 }
 
-char	**ft_split(char const *s, char c)
+void    make_split(char **split, char *s, char c)
 {
-	char	**splitted;
+    size_t  i;
+    char    *next;
 
-	if (!s || !*s)
-		return (ft_calloc(1, sizeof(char **)));
-	splitted = find_words(s, c);
-	if (splitted)
-		split_words(splitted, s, c);
-	return (splitted);
+    i = 0;
+    next = NULL;
+    while (1)
+    {
+        next = ft_strchr(s, c);
+        if (next && (next - s) > 0 && ft_strlen(s) > (size_t)(next - s))
+        {
+            split[i++] = ft_strndup(s, (next - s));
+            s += (next - s) + 1;
+            continue ;
+        }
+        if (next_bit(&s, next))
+            continue ;
+        if (ft_strlen(s))
+            split[i++] = ft_strdup(s);
+        break ;
+    }
+}
+
+char    **ft_split(char const *s, char c)
+{
+    size_t      size;
+    char        **split;
+
+    size = 0;
+    if (!s || !*s)
+        return (ft_calloc(1, sizeof(char **)));
+    size = count_words((char *)s, c, size);
+    split = (char **) ft_calloc(size + 1, sizeof(char *));
+    if (!split)
+        return (split);
+    make_split(split, (char *)s, c);
+    return (split);
 }
